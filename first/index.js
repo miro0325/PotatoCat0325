c4b006992e0a094b84aaf8e33d3068f25296ad1e
miro0325 <hanc02095@gmail.com>
2024-03-18 15:04:05 +0900

    a

--- first/index.js
+++ first/index.js
@@ -49,6 +49,9 @@ const enemy = new Sprite({
 
 function animate() {
     window.requestAnimationFrame(animate);
+    c.fillStyle = "black";
+    c.fillRect(0,0, canvas.width,canvas.height);
+
     player.update();
     enemy.update();
 }
