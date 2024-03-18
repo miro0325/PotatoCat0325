2c27203e240202c93d3c9aedd953b3aa1b38aed5
miro0325 <hanc02095@gmail.com>
2024-03-18 15:35:35 +0900

    Bug FIx

--- first/index.js
+++ first/index.js
@@ -24,7 +24,7 @@ class Sprite {
     update() {
         this.draw();
 
-        this.position += this.velocity;
+        this.position.y += this.velocity.y;
         if(this.position.y + this.height + this.velocity.y >= canvas.height) {
             this.velocity.y = 0;
         } else {
